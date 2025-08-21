fec27480184ade6e7d7ef336a399f590d2857296
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-08-21 00:39:21 -0400

    feat: Added a backend abstract class

---
+++ pyagentic/llm/_backend.py
@@ -0,0 +1,29 @@
+from typing import List, Optional, Type
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+
+from pyagentic._base._tool import _ToolDefinition
+from pyagentic._base._context import _AgentContext
+from pyagentic.models.llm import Message, Response
+
+
+class LLMBackend(ABC):
+    __llm_name__ = "backend"
+    __supports_tool_calls__ = True
+    __supports_structured_outputs__ = True
+
+    @abstractmethod
+    def __init__(self, model: str, api_key: str, *, base_url: str = False, **kwargs): ...
+
+    @abstractmethod
+    def to_tool_call_message(self, result, id_) -> Message: ...
+
+    @abstractmethod
+    async def generate(
+        self,
+        context: _AgentContext,
+        *,
+        tool_defs: Optional[List[_ToolDefinition]] = None,
+        response_format: Optional[Type[BaseModel]] = None,
+        **kwargs,
+    ) -> Response: ...
