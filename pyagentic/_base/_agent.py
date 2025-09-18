851a2b9923b963a45e2b05670233d03bbf341e35
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-09-17 21:53:55 -0400

    feat: Added suppport for more providers, such as anthropic

--- pyagentic/_base/_agent.py
+++ pyagentic/_base/_agent.py
@@ -162,7 +162,7 @@ class Agent(metaclass=AgentMeta):
             )
 
         if self.__response_format__ and not self.provider.__supports_structured_outputs__:
-            raise Exception("Response format is not support with this provider")
+            raise Exception("Response format is not supported with this provider")
 
         if self.__tool_defs__ and not self.provider.__supports_tool_calls__:
             raise Exception("Tools are not supported with this provider")
