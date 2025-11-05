a909a64d3b1a2f623bdf70f653e72b9a04b78d34
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-11-05 12:56:23 -0500

    fix: Changed tool def failed to invalid tool def exception

--- pyagentic/_base/_exceptions.py
+++ pyagentic/_base/_exceptions.py
@@ -1,10 +1,10 @@
 from typing import Literal
 
 
-class ToolDeclarationFailed(Exception):
+class InvalidToolDefinition(Exception):
 
     def __init__(self, tool_name, message):
-        message = f"Tool declaration failed for {tool_name}" f"{message}"
+        message = f"Invalid Tool Definition: {tool_name}: " f"{message}"
         super().__init__(message)
 
 
